34fcc190acd7c95d57df9b5a9e92e7b1f4ba9eb4
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-06 23:52:54 +0600

    local database connected

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect()
+mongoose.connect('mongodb://localhost:27017/firstDB',{
+    useNewUrlParser:true,useUnifiedTopology:true
+})
+.then(()=>{console.log("database connection")})
+.catch((err)=>{console.log(err)})
