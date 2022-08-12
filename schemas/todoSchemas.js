4134bbc9227e363be0bb5d8dc1f0b7e1cd4c3762
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-12 21:50:16 +0600

    schema

--- schemas/todoSchemas.js
+++ schemas/todoSchemas.js
@@ -3,5 +3,15 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const todoSchema = new Schema({
-    
+    title:{
+        type:String,
+        required: true,
+
+    },
+    description: String,
+    status:{
+        type: String,
+        enum:['active','inactive']
+    }
+
 })
