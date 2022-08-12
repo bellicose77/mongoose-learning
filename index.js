056146c4bd4549e00255de30d4a7c043edae4eb4
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-12 20:31:23 +0600

    mongoss crud

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 
 const app = express();
-app.use(express.json())
+const port = 5000;
+app.use(express.json());
 
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost:27017/firstDB',{
+mongoose.connect('mongodb://localhost:27017/TODO',{
     useNewUrlParser:true,useUnifiedTopology:true
 })
 .then(()=>{console.log("database connection")})
 .catch((err)=>{console.log(err)})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
 
 
 
