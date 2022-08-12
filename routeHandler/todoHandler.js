c1b1796aedbd8f4f5c35b5441f1881acb9cd119d
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-12 21:27:59 +0600

    route

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -15,5 +15,14 @@ router.get('/:id', async(req,res)=>{
 
 //post a single todo 
 router.post('/',async(req,res)=>{
-    
+
+});
+//post multiple todo
+router.post('/all',async(req,res)=>{
+
+});
+
+// update a todo
+router.put('/',async(req,res)=>{
+
 })
