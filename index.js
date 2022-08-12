6bc2077e3ac52cf0ae7c9a354ebd472c903016a0
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-12 20:26:46 +0600

    mongoose crud

--- index.js
+++ index.js
@@ -8,43 +8,6 @@ mongoose.connect('mongodb://localhost:27017/firstDB',{
 .then(()=>{console.log("database connection")})
 .catch((err)=>{console.log(err)})
 
-const blogSchema = new Schema({
-    name:{
-        type : String,
-        required : true
-    },
-    ctype : String,
-    videos : Number,
-    author: String,
-    active: Boolean,
-    date: {
-        type : Date,
-        default : Date.now
-    }
 
-
-});
-
-const Blog = new mongoose.model('Blog',blogSchema);
-
-// const firstBlog = new Blog({
-//     name:"Nodejs",
-//     ctype : "Learning fast",
-//     videos : 40,
-//     author: "tpr na",
-//     active: true,
-
-// });
-
-
-
-//Blog.insertMany([secondBlog, thirdBlog,fourthBlog ])
-
-const readDocu = async () =>{
-   //const result = await Blog.find({name:'Reacjs'});
-//    const result = await Blog.find({name:{$in:['ReactJs','NodeJs','Expressjs']}})
-    const result = await Blog.find({$nor:[{name:"Reacjs"},{video:50}]})
-   console.log(result);
-}
 readDocu();
 
